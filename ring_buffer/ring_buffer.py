de8ca17027ac6c92f86d6c3d14c61f8d080f15af
Melissa Murphy <melissa@thoughtfulmonkey.design>
2020-02-28 10:00:22 -0700

    ring buffer mvp done

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,13 +8,30 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        if self.current is None:
+            # if append is called and the current ringbuffer is empty
+            # then current is first item in storage
+            self.current = self.storage.head
+
+        if self.storage.length == self.capacity:
+            # if the contents of storage reach the set capacity
+            # then the current value is set to item
+            # and current becomes the next oldest item
+            self.current.value = item
+            self.current = self.current.next
+
+        else:
+            self.storage.add_to_tail(item)
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
         # TODO: Your code here
+        node = self.storage.head
+        while node:
+            list_buffer_contents.append(node.value)
+            node = node.next
 
         return list_buffer_contents
 
